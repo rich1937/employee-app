7451ab81a1f89c2b19a81d53d9435e3028e12c65
rich1937 <ruchit.kapl@gmail.com>
2020-08-14 00:35:18 -0400

    used react bootstrap grid to display the details of departments page

--- src/components/Department.js
+++ src/components/Department.js
@@ -1,12 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Table} from 'react-bootstrap';
 
 export class Department extends Component {
 
+    constructor(props)
+    {
+    super(props);
+    this.state = {deps: []}
+    }
+
+    refreshList(){
+        this.setState({
+             deps:[{"DepartmentID":1, "DepartmentName":"IT"},{"DepartmentID":2, "DepartmentName":"Support"}]
+            })
+    }
+
+    componentDidMount() {
+        this.refreshList();
+    }
+
     render() {
+        const{deps} = this.state;
         return (
-            <div className="mt-5 d-flex justify-content-left">
-                <h3>Welcome to Department page.</h3>
-            </div>
+            <Table className="mt-4" striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                    <th>DepartmentID</th>
+                    <th>DepartmentName</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {deps.map(dep=>
+                    <tr key={dep.DepartmentID}>
+                    <td>{dep.DepartmentID}</td>
+                    <td>{dep.DepartmentName}</td>
+                    </tr>
+                    )}
+                </tbody>
+            </Table>
         )
     }
 
